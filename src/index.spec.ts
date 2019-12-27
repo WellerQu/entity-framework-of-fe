982109892c290b681892cd8d8232ba1d7d0086af
nix <qiuwei@hualala.com>
2019-12-27 14:57:58 +0800

    feat: 补充测试用例

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -58,4 +58,288 @@ describe('Behavior-driven development', () => {
     expect(bar).toHaveProperty('name', 'ba1')
     expect(bar).toHaveProperty('age', 13)
   })
+
+  it('query a foo with a foreign key bar(id, name), the relationship is one to one', async () => {
+    @EF.behavior('load', 'http://localhost:3000/bar?id=$id&name=$name', 'GET', (...a: any[]) => a, (a: any[]) => a[0])
+    class Bar {
+      @EF.primary()
+      @EF.member()
+      id: number = 0
+
+      @EF.primary()
+      @EF.member()
+      name: string = ''
+
+      @EF.member()
+      age: number = 0
+    }
+
+    @EF.behavior('load', 'http://localhost:3000/foo/$id', 'GET')
+    class Foo {
+      @EF.primary()
+      @EF.member()
+      id: number = 0
+
+      @EF.member('name')
+      aliasName: string = ''
+
+      @EF.foreign(Bar, 'bar', 'id')
+      @EF.member()
+      bid: number = 0
+
+      @EF.foreign(Bar, 'bar', 'name')
+      @EF.member()
+      bName: string = ''
+
+      @EF.navigator(EF.Relationship.One, 'bar')
+      bar?: Bar
+    }
+
+    class Context extends EF.EntityContext {
+      @EF.set()
+      foo = new EF.EntitySet<Foo>(this, Foo)
+      @EF.set()
+      bar = new EF.EntitySet<Bar>(this, Bar)
+    }
+
+    const ctx = new Context()
+    await ctx.foo.include('bar').load(1)
+    const foo = ctx.foo.find(1)
+    expect(foo).toHaveProperty('id', 1)
+    expect(foo!.bar).toBeDefined()
+    expect(foo!.bar).toHaveProperty('id', 1)
+    expect(foo!.bar).toHaveProperty('name', 'ba1')
+    expect(foo!.bar).toHaveProperty('age', 13)
+    expect(ctx.foo.size).toEqual(1)
+    expect(ctx.bar.size).toEqual(1)
+  })
+
+  it('query a foo with a foreign key jar(id), the relationship is one to many', async () => {
+    @EF.behavior('load', 'http://localhost:3000/jar/$id', 'GET')
+    class Jar {
+      @EF.primary()
+      @EF.member()
+      id: number = 0
+
+      @EF.member()
+      name: string = ''
+    }
+
+    @EF.behavior('load', 'http://localhost:3000/foo/$id', 'GET')
+    class Foo {
+      @EF.primary()
+      @EF.member()
+      id: number = 0
+
+      @EF.member('name')
+      aliasName: string = ''
+
+      @EF.foreign(Jar, 'jar', 'id')
+      @EF.member()
+      jid: number[] = []
+
+      @EF.navigator(EF.Relationship.Many, 'jar')
+      jar?: Jar[]
+    }
+
+    class Context extends EF.EntityContext {
+      @EF.set()
+      foo = new EF.EntitySet<Foo>(this, Foo)
+      @EF.set()
+      jar = new EF.EntitySet<Jar>(this, Jar)
+    }
+
+    const ctx = new Context()
+    await ctx.foo.include('jar').load(1)
+    const foo = ctx.foo.find(1)
+    expect(foo).toHaveProperty('id', 1)
+    expect(foo!.jar).toBeDefined()
+    expect(foo!.jar).toHaveLength(2)
+    expect(foo!.jar).toEqual([{ id: 1, name: 'ja1' }, { id: 2, name: 'ja2' }])
+    expect(ctx.foo.size).toEqual(1)
+    expect(ctx.jar.size).toEqual(2)
+  })
+
+  it('query a foo with two foreign keys bar(id, name) and jar(id)', async () => {
+    @EF.behavior('load', 'http://localhost:3000/jar/$id', 'GET')
+    class Jar {
+      @EF.primary()
+      @EF.member()
+      id: number = 0
+
+      @EF.member()
+      name: string = ''
+    }
+
+    @EF.behavior('load', 'http://localhost:3000/bar?id=$id&name=$name', 'GET', (...a: any[]) => a, (a: any[]) => a[0])
+    class Bar {
+      @EF.primary()
+      @EF.member()
+      id: number = 0
+
+      @EF.primary()
+      @EF.member()
+      name: string = ''
+
+      @EF.member()
+      age: number = 0
+    }
+
+    @EF.behavior('load', 'http://localhost:3000/foo/$id', 'GET')
+    class Foo {
+      @EF.primary()
+      @EF.member()
+      id: number = 0
+
+      @EF.member('name')
+      aliasName: string = ''
+
+      @EF.foreign(Jar, 'jar', 'id')
+      @EF.member()
+      jid: number[] = []
+
+      @EF.navigator(EF.Relationship.Many, 'jar')
+      jar?: Jar[]
+
+      @EF.foreign(Bar, 'bar', 'id')
+      @EF.member()
+      bid: number = 0
+
+      @EF.foreign(Bar, 'bar', 'name')
+      @EF.member()
+      bName: string = ''
+
+      @EF.navigator(EF.Relationship.One, 'bar')
+      bar?: Bar
+    }
+
+    class Context extends EF.EntityContext {
+      @EF.set()
+      foo = new EF.EntitySet<Foo>(this, Foo)
+      @EF.set()
+      jar = new EF.EntitySet<Jar>(this, Jar)
+      @EF.set()
+      bar = new EF.EntitySet<Bar>(this, Bar)
+    }
+
+    const ctx = new Context()
+    await ctx.foo.include('jar').include('bar').load(1)
+    const foo = ctx.foo.find(1)
+    expect(foo).toHaveProperty('id', 1)
+    expect(foo!.bar).toBeDefined()
+    expect(foo!.bar).toHaveProperty('id', 1)
+    expect(foo!.bar).toHaveProperty('name', 'ba1')
+    expect(foo!.bar).toHaveProperty('age', 13)
+    expect(foo!.jar).toBeDefined()
+    expect(foo!.jar).toHaveLength(2)
+    expect(foo!.jar).toEqual([{ id: 1, name: 'ja1' }, { id: 2, name: 'ja2' }])
+    expect(ctx.foo.size).toEqual(1)
+    expect(ctx.bar.size).toEqual(1)
+    expect(ctx.jar.size).toEqual(2)
+  })
+
+  it('query a foo with a foreign key bar(id, name), and cascading query to zar(id) by bar', async () => {
+    @EF.behavior('load', 'http://localhost:3000/zar/$id', 'GET')
+    class Zar {
+      @EF.primary()
+      @EF.member()
+      id: number = 0
+
+      @EF.member()
+      name: string = ''
+    }
+
+    @EF.behavior('load', 'http://localhost:3000/bar?id=$id&name=$name', 'GET', (...a: any[]) => a, (a: any[]) => a[0])
+    class Bar {
+      @EF.primary()
+      @EF.member()
+      id: number = 0
+
+      @EF.primary()
+      @EF.member()
+      name: string = ''
+
+      @EF.member()
+      age: number = 0
+
+      @EF.foreign(Zar, 'zar', 'id')
+      @EF.member()
+      zid: number = 0
+
+      @EF.navigator(EF.Relationship.One, 'zar')
+      zar?: Zar
+    }
+
+    @EF.behavior('load', 'http://localhost:3000/foo/$id', 'GET')
+    class Foo {
+      @EF.primary()
+      @EF.member()
+      id: number = 0
+
+      @EF.member('name')
+      aliasName: string = ''
+
+      @EF.foreign(Bar, 'bar', 'id')
+      @EF.member()
+      bid: number = 0
+
+      @EF.foreign(Bar, 'bar', 'name')
+      @EF.member()
+      bName: string = ''
+
+      @EF.navigator(EF.Relationship.One, 'bar')
+      bar?: Bar
+    }
+
+    class Context extends EF.EntityContext {
+      @EF.set()
+      foo = new EF.EntitySet<Foo>(this, Foo)
+      @EF.set()
+      bar = new EF.EntitySet<Bar>(this, Bar)
+      @EF.set()
+      zar = new EF.EntitySet<Zar>(this, Zar)
+    }
+
+    const ctx = new Context()
+    await ctx.foo.include('bar').include('zar').load(1)
+    const foo = ctx.foo.find(1)
+    expect(foo).toHaveProperty('id', 1)
+    expect(foo!.bar).toBeDefined()
+    expect(foo!.bar).toHaveProperty('id', 1)
+    expect(foo!.bar).toHaveProperty('name', 'ba1')
+    expect(foo!.bar).toHaveProperty('age', 13)
+    expect(foo!.bar!.zar).toBeDefined()
+    expect(foo!.bar!.zar).toHaveProperty('id', 1)
+    expect(foo!.bar!.zar).toHaveProperty('name', 'za1')
+    expect(ctx.foo.size).toEqual(1)
+    expect(ctx.bar.size).toEqual(1)
+    expect(ctx.zar.size).toEqual(1)
+  })
+
+  it('there is not any side-effect between two load behaviors', async () => {
+    @EF.behavior('load', 'http://localhost:3000/foo/$id', 'GET')
+    class Foo {
+      @EF.primary()
+      @EF.member()
+      id: number = 0
+
+      @EF.member('name')
+      aliasName: string = ''
+    }
+
+    class Context extends EF.EntityContext {
+      @EF.set()
+      foo = new EF.EntitySet<Foo>(this, Foo)
+    }
+
+    const ctx = new Context()
+    await ctx.foo.load(1)
+    const foo1 = ctx.foo.find(1)
+
+    await ctx.foo.load(2)
+    const foo2 = ctx.foo.find(2)
+
+    expect(foo1).not.toEqual(foo2)
+    expect(ctx.foo.size).toEqual(1)
+  })
 })

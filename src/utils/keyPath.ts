6adbdd206f54aa4a2b1f712b4c80c049b1fc81ae
nix <qiuwei@hualala.com>
2020-05-21 11:16:13 +0800

    fix: 修复keyPathSetter在嵌套对象上写入值出错的情况

--- src/utils/keyPath.ts
+++ src/utils/keyPath.ts
@@ -1,3 +1,5 @@
+type Store = { [key: string]: any }
+
 export const keyPathGetter = <T = any>(keyPath: string, defaultValue?: T) => {
   return (target: Store) => {
     return (keyPath
@@ -9,15 +11,15 @@ export const keyPathGetter = <T = any>(keyPath: string, defaultValue?: T) => {
   }
 }
 
-export const keyPathSetter = <T= any>(keyPath: string) => {
+export const keyPathSetter = <T = any>(keyPath: string) => {
   return (target: Store, value: T) => {
     return keyPath.split('.').reduce((middle: Store, key: string, index: number, keys: string[]) => {
-      if (index === keys.length - 1) {
-        middle[key] = value
-      }
-
       if (!middle[key]) {
         middle[key] = {}
+      }
+
+      if (index === keys.length - 1) {
+        middle[key] = value
       }
 
       return middle[key]
